456786c5b05bc5e46f1cfbdb96facd0c124d8cc3
Treasure Uzoma <treasureuzoma650@gmail.com>
2024-09-24 09:03:53 +0100

    quick fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,16 +4,18 @@ const Hero = () => {
             <div className="my_fixed_width_2">
                 <h1 className="text-4xl font-[800] my-2">
                     Ask me anything (
-                    <span className="text-[#DA0037] animate-pulse">
+                    <span className="text-[#DA0037]">
                         literally
                     </span>
                     )
                 </h1>
                 <p className="text-base my-2">
-                    Ask anonymously, get weirdly honest answers, and uncover unexpected truths.
+                    Ask anonymously, get weirdly honest answers, and uncover
+                    unexpected truths.
                 </p>
                 <a
-                    href="#" target="_blank"
+                    href="#"
+                    target="_blank"
                     className="inline-flex items-center justify-between bg-[#DA0037] px-5 py-3 rounded-3xl text-[0.78rem] font-bold w-[8rem] text-black my-7 transition-transform duration-300 hover:scale-105"
                 >
                     <span>Start Now</span>
